6b0df3b14f78f04020f147fb60ad03766ed69384
Jakub Kret <jakub.a.kret@gmail.com>
2018-04-26 23:11:34 +0200

    Use dotenv in index.js

--- src/index.js
+++ src/index.js
@@ -2,5 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import AppProvider from './Provider';
+import dotenv from 'dotenv';
+dotenv.config();
 
 ReactDOM.render(<AppProvider />, document.getElementById('root'));
