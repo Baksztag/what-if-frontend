350a82df54cd1424132ae7bd49238b1cd23a1a6f
Jakub Kret <jakub.a.kret@gmail.com>
2018-04-26 23:22:21 +0200

    Add firebase dependency

---
+++ src/Auth.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+class AuthProvider extends Component {
+    render() {
+        return (
+            <div></div>
+        );
+    }
+}
+
+AuthProvider.propTypes = {};
+AuthProvider.defaultProps = {};
+
+export default AuthProvider;
