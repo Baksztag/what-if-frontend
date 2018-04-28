eea85ec68c291881601e051d53d10c3e2351f5a7
Baksztag <jakub.a.kret@gmail.com>
2018-04-28 15:16:40 +0200

    Move firebase initialization to services directory

--- src/index.js
+++ src/index.js
@@ -2,17 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import AppProvider from './providers';
-import firebase from 'firebase';
+import {initializeFirebase} from './services/firebase';
 import dotenv from 'dotenv';
 dotenv.config();
-const config = {
-    apiKey: process.env.REACT_APP_API_KEY,
-    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-    databaseURL: process.env.REACT_APP_DATABASE_URL,
-    projectId: process.env.REACT_APP_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-};
-firebase.initializeApp(config);
+initializeFirebase();
 
 ReactDOM.render(<AppProvider />, document.getElementById('root'));
