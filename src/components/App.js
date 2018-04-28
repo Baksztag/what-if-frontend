457704452091e9bca7756fa58aba28689c232568
Baksztag <jakub.a.kret@gmail.com>
2018-04-28 14:55:33 +0200

    Enable dispatching action in App component

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
+import {connect} from 'react-redux';
 import logo from './logo.svg';
 import './App.css';
+
+import {getTest} from '../actions';
 
 import {LogoutButton} from '.';
 
 class App extends Component {
   render() {
+      console.log(this.props)
+      const {getTest} = this.props;
+
     return (
       <div className="App">
         <header className="App-header">
@@ -16,9 +22,23 @@ class App extends Component {
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
           <LogoutButton/>
+          <button onClick={getTest}>test</button>
       </div>
     );
   }
 }
 
-export default App;
+function mapStateToProps({isFetching, test}, ownProps) {
+    return {
+        isFetching,
+        test,
+    }
+}
+
+function mapDispatchToProps(dispatch, ownProps) {
+    return {
+        getTest: () => dispatch(getTest()),
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
