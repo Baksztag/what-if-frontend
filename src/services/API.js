111ef98318e6e4a35fb1ea289302610db66dfcb6
Baksztag <jakub.a.kret@gmail.com>
2018-05-05 11:20:43 +0200

    Use getIdToken for authorization

--- src/services/API.js
+++ src/services/API.js
@@ -1,21 +1,41 @@
 import axios from 'axios';
+import firebase from 'firebase';
 
 const API_URL = 'http://localhost:4000';
 
-function getUserToken() {
-    return `Bearer ${localStorage.getItem("AUTH_TOKEN")};`
-}
+// function getUserToken() {
+    // return `Bearer ${localStorage.getItem("AUTH_TOKEN")};`
+    // return firebase.auth().currentUser.getIdToken(true)
+        // .then(idToken => {
+        //     console.log(idToken)
+        //     return `Bearer ${idToken}`;
+        // })
+        // .catch(error => {
+        //     console.log(error)
+        //     return '';
+        // });
+// }
 
 function saveUserToken(token) {
     localStorage.setItem("AUTH_TOKEN", token);
 }
 
 function get(url) {
-    return axios.get(`${API_URL}${url}`, {headers: {Authorization: getUserToken()}})
+    return firebase.auth().currentUser.getIdToken(true)
+        .then(idToken => {
+            return axios.get(`${API_URL}${url}`, {headers: {Authorization: `Bearer ${idToken}`}})
+        })
+        .catch(error => {
+            return new Promise((resolve, reject) => {
+                console.log(error)
+                reject()
+            })
+        })
+
 }
 
 export default {
     get,
-    getUserToken,
+    // getUserToken,
     saveUserToken
 };
