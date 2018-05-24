50b9e0d3c901d54c897ec479a45274d91a5478cb
Jakub Kret <jakub.a.kret@gmail.com>
2018-05-24 13:37:37 +0200

    Add temporary changes to API

--- src/services/API.js
+++ src/services/API.js
@@ -1,12 +1,17 @@
 import axios from 'axios';
 import firebase from 'firebase';
 
-const API_URL = 'http://localhost:4000';
-const WS_URL = 'ws://localhost:4000/s';
+// const API_URL = 'http://localhost:4000';
+const API_URL = 'http://172.20.10.2:4000';
+// const WS_URL = 'ws://localhost:4000/s';
+const WS_URL = 'ws://172.20.10.2:4000/s';
 
 function getUserToken() {
+    // console.log(firebase.auth().currentUser)
     return firebase.auth().currentUser.getIdToken(true)
-        .then(idToken => idToken)
+        // .then(idToken => idToken)
+    //TODO replace with actual token
+        .then(idToken => firebase.auth().currentUser.uid)
         .catch(error => error)
 }
 
@@ -30,6 +35,8 @@ function saveUserToken(token) {
 function get(url) {
     return firebase.auth().currentUser.getIdToken(true)
         .then(idToken => {
+            //TODO replace with actual token
+            idToken = firebase.auth().currentUser.uid
             return axios.get(`${API_URL}${url}`, {headers: {Authorization: `Bearer ${idToken}`}})
         })
         .catch(error => {
@@ -38,12 +45,28 @@ function get(url) {
                 reject()
             })
         })
+}
 
+function post(url, data) {
+    return firebase.auth().currentUser.getIdToken(true)
+        .then(idToken => {
+            //TODO replace with actual token
+            idToken = firebase.auth().currentUser.uid
+            return axios.post(`${API_URL}${url}`,
+                data,
+                {
+                    headers: {
+                        'Authorization': `Bearer ${idToken}`,
+                        'Content-Type': 'application/json',
+                    }
+                })
+        })
 }
 
 export default {
     get,
     getUserToken,
+    post,
     saveUserToken,
     WS_URL,
 };
