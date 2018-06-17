e544f9292f48a4c5c94e223d41d2563f61598f3e
Jakub Kret <jakub.a.kret@gmail.com>
2018-06-17 20:46:26 +0200

    Remove dotenv dependency

--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import AppProvider from './providers';
 import {initializeFirebase} from './services';
-import dotenv from 'dotenv';
-dotenv.config();
+// import dotenv from 'dotenv';
+// dotenv.config();
 initializeFirebase();
 
 ReactDOM.render(<AppProvider />, document.getElementById('root'));
