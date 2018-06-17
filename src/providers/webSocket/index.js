211ce22f4efce27c7063d4b4af8dc7f9cbbc7f3b
Jakub Kret <jakub.a.kret@gmail.com>
2018-06-17 20:10:32 +0200

    Move phoenix-channels directly to src
    Move phoenix-channels code to project source directory due to issues with heroku build

--- src/providers/webSocket/index.js
+++ src/providers/webSocket/index.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {Socket} from 'phoenix-channels';
+import {Socket} from '../../phoenix-channels';
 import {BrowserRouter} from 'react-router-dom';
 
 import AppRouter from '../Router';
